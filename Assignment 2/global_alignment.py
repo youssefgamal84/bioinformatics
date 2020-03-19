0db0f9f27740ce957fe3bba26e289b4f0109759b
Joe <youssefgamal84@yahoo.com>
2020-03-19 22:14:05 +0200

    Added assignment 2

--- Assignment 2/global_alignment.py
+++ Assignment 2/global_alignment.py
@@ -23,23 +23,29 @@ def global_alignment(strand1, strand2, match, mismatch, gap):
     C = len(strand2) + 1
 
     matrix = np.zeros((R, C), dtype=np.int)
+    # indicating arrows for traceback
     left_arrows = np.zeros((R, C), dtype=np.bool)
     up_arrows = np.zeros((R, C), dtype=np.bool)
     diagonal_arrows = np.zeros((R, C), dtype=np.bool)
 
     # fill the matrix
+
+    ## initialization
     for i in range(0, C):
         matrix[0, i] = i * gap
         left_arrows[0, i] = True
     for i in range(0, R):
         matrix[i, 0] = i * gap
         up_arrows[i, 0] = True
+
     for i in range(1, R):
         for j in range(1, C):
             is_match = (strand1[i - 1] == strand2[j - 1])
+            # Determine the highest score
             new_score = max(matrix[i - 1][j - 1] + is_match * match + (not is_match) * mismatch, matrix[i - 1][j] + gap,
                             matrix[i][j - 1] + gap)
             matrix[i][j] = new_score
+            # determine what paths made the highest score
             if new_score == matrix[i - 1][j - 1] + is_match * match + (not is_match) * mismatch:
                 diagonal_arrows[i][j] = True
             if new_score == matrix[i - 1][j] + gap:
